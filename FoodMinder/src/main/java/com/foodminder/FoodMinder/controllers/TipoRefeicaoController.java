93a804c96d1c35388e260af6507b7808ab777125
renatonunes74 <renato.s.nunes74@gmail.com>
2023-08-04 17:20:02 -0300

    feat: POST e DELETE por ID de Tipo de Refeições

--- FoodMinder/src/main/java/com/foodminder/FoodMinder/controllers/TipoRefeicaoController.java
+++ FoodMinder/src/main/java/com/foodminder/FoodMinder/controllers/TipoRefeicaoController.java
@@ -1,19 +1,38 @@
 package com.foodminder.FoodMinder.controllers;
 
-import com.foodminder.FoodMinder.domain.TipoRefeicao.TipoRefeicaoRepository;
+import com.foodminder.FoodMinder.domain.tipoRefeicao.RequestTipoRefeicao;
+import com.foodminder.FoodMinder.domain.tipoRefeicao.TipoRefeicao;
+import com.foodminder.FoodMinder.domain.tipoRefeicao.TipoRefeicaoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/tipoRefeicoes")
+@RequestMapping("/tipoRefeicao")
 public class TipoRefeicaoController {
     @Autowired
     private TipoRefeicaoRepository repository;
     @GetMapping
     public ResponseEntity getAllTipoRefeicao() {
         return ResponseEntity.ok(repository.findAll());
+    }
+    @PostMapping
+    public ResponseEntity registerTipoRefeicao(@RequestBody @Valid RequestTipoRefeicao data) {
+        TipoRefeicao newTipoRefeicao = new TipoRefeicao(data);
+        repository.save(newTipoRefeicao);
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteTipoRefeicao(@PathVariable Integer id){
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            throw new EntityNotFoundException();
+        }
     }
 }
