3d6fba0d8d4ad364cb08fa99b68b05b7464c5fa6
renatonunes74 <renato.s.nunes74@gmail.com>
2023-08-05 22:25:35 -0300

    refactor/feat: teste unitários mais abrangentes com o pitest

--- FoodMinder/src/test/java/com/foodminder/FoodMinder/RefeicaoControllerTest.java
+++ FoodMinder/src/test/java/com/foodminder/FoodMinder/RefeicaoControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
@@ -33,10 +40,20 @@ public class RefeicaoControllerTest {
 
     @BeforeEach
     public void setup() {
-        Refeicao refeicao = new Refeicao();
-        refeicao.setNome("Lasanha");
-        refeicao.setReceita("lasanha.md");
-        refeicaoRepository.save(refeicao);
+        Refeicao refeicao1 = new Refeicao();
+        refeicao1.setId(1);
+        refeicao1.setNome("Lasanha");
+        refeicao1.setReceita("lasanha.md");
+
+        Refeicao refeicao2 = new Refeicao();
+        refeicao2.setId(2);
+        refeicao2.setNome("Torta de maçã");
+        refeicao2.setReceita("torta_de_maçã.md");
+
+        List<Refeicao> refeicoes = List.of(refeicao1, refeicao2);
+        when(refeicaoRepository.findAll()).thenReturn(refeicoes);
+        when(refeicaoRepository.findById(1)).thenReturn(Optional.of(refeicao1));
+
     }
     @AfterEach
     public void down() {refeicaoRepository.deleteAll();}
@@ -45,21 +62,62 @@ public class RefeicaoControllerTest {
     public void RefeicaoController_GetAllRefeicao_ReturnOk() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/refeicao"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].nome").value("Lasanha"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].receita").value("lasanha.md"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].nome").value("Torta de maçã"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].receita").value("torta_de_maçã.md"))
                 .andDo(MockMvcResultHandlers.print());
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 11 })
+    @DisplayName("Listar refeição por ID; 1 para OK, 11 para NOT FOUND")
+    public void RefeicaoController_GetById_VerifyStatus(int id) throws Exception {
+        if (id == 1) {
+            mockMvc.perform(MockMvcRequestBuilders.get("/refeicao/" + id))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value("Lasanha"))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.receita").value("lasanha.md"))
+                    .andDo(MockMvcResultHandlers.print());
+        } else if (id == 11) {
+            mockMvc.perform(MockMvcRequestBuilders.get("/refeicao/" + id))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andDo(MockMvcResultHandlers.print());
+        }
     }
     @Test
     @DisplayName("Registrar refeição")
     public void RefeicaoController_RegisterRefeicao_ReturnCreated() throws Exception {
         Refeicao refeicao = Refeicao.builder()
-                .nome("Torta de maçã")
-                .receita("torta_de_maçã.md")
+                .nome("Torta de Pêssego")
+                .receita("torta_de_pêssego.md")
                 .build();
         String refeicaoRequest = objectMapper.writeValueAsString(refeicao);
         mockMvc.perform(MockMvcRequestBuilders.post("/refeicao")
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8")
                 .content(refeicaoRequest))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value("Torta de Pêssego"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.receita").value("torta_de_pêssego.md"))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andDo(MockMvcResultHandlers.print());
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 11 })
+    @DisplayName("Deletar refeição por ID; 1 para OK, 11 para NOT FOUND")
+    public void RefeicaoControleer_DeleteById_VerifyStatus(int id) throws Exception{
+        if (id == 1) {
+            mockMvc.perform(MockMvcRequestBuilders.delete("/refeicao/" + id))
+                    .andExpect(MockMvcResultMatchers.status().isNoContent())
+                    .andDo(MockMvcResultHandlers.print());
+        } else if (id == 11) {
+            mockMvc.perform(MockMvcRequestBuilders.delete("/refeicao/" + id))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andDo(MockMvcResultHandlers.print());
+        }
     }
 }
--- FoodMinder/src/test/java/com/foodminder/FoodMinder/TipoRefeicaoControllerTest.java
+++ FoodMinder/src/test/java/com/foodminder/FoodMinder/TipoRefeicaoControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
@@ -32,20 +39,48 @@ public class TipoRefeicaoControllerTest {
     private TipoRefeicaoRepository tipoRefeicaoRepository;
     @BeforeEach
     public void setup() {
-        tipoRefeicao = new TipoRefeicao();
-        tipoRefeicao.setTipo("Café da manhã");
-        tipoRefeicaoRepository.save(tipoRefeicao);
+        TipoRefeicao tipoRefeicao1 = new TipoRefeicao();
+        tipoRefeicao1.setId(1);
+        tipoRefeicao1.setTipo("Café da manhã");
+
+        TipoRefeicao tipoRefeicao2 = new TipoRefeicao();
+        tipoRefeicao2.setId(2);
+        tipoRefeicao2.setTipo("Almoço");
+
+        List<TipoRefeicao> tipoRefeicoes = List.of(tipoRefeicao1, tipoRefeicao2);
+        when(tipoRefeicaoRepository.findAll()).thenReturn(tipoRefeicoes);
+        when(tipoRefeicaoRepository.findById(1)).thenReturn(Optional.of(tipoRefeicao1));
     }
     @AfterEach
-    public void down() {
-        tipoRefeicaoRepository.deleteAll();
-    }
+    public void down() {tipoRefeicaoRepository.deleteAll();}
     @Test
     @DisplayName("Listar todas os tipos de refeições")
     public void TipoRefeicaoController_GetAllTipoRefeicao_ReturnOk() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/tipoRefeicao"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].tipo").value("Café da manhã"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].tipo").value("Almoço"))
                 .andDo(MockMvcResultHandlers.print());
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 11 })
+    @DisplayName("Listar tipo de refeição por ID; 1 para OK, 11 para NOT FOUND")
+    public void TipoRefeicaoController_GetById_VerifyStatus(int id) throws Exception {
+        if (id == 1) {
+            mockMvc.perform(MockMvcRequestBuilders.get("/tipoRefeicao/" + id))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.tipo").value("Café da manhã"))
+                    .andDo(MockMvcResultHandlers.print());
+        } else if (id == 11) {
+            mockMvc.perform(MockMvcRequestBuilders.get("/tipoRefeicao/" + id))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andDo(MockMvcResultHandlers.print());
+        }
     }
     @Test
     @DisplayName("Registrar tipo de refeição")
@@ -59,6 +94,22 @@ public class TipoRefeicaoControllerTest {
                 .characterEncoding("UTF-8")
                 .content(tipoRefeicaoRequest))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.tipo").value("Café da Tarde"))
                 .andDo(MockMvcResultHandlers.print());
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 11 })
+    @DisplayName("Deletar tipo de refeição por ID; 1 para OK, 11 para NOT FOUND")
+    public void TipoRefeicaoController_DeleteById_VerifyStatus(int id) throws Exception {
+        if(id == 1) {
+            mockMvc.perform(MockMvcRequestBuilders.delete("/tipoRefeicao/" + id))
+                    .andExpect(MockMvcResultMatchers.status().isNoContent())
+                    .andDo(MockMvcResultHandlers.print());
+        }
+        else if (id == 11) {
+            mockMvc.perform(MockMvcRequestBuilders.delete("/tipoRefeicao/" + id))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andDo(MockMvcResultHandlers.print());
+        }
     }
 }
